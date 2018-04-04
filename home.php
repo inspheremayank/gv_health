335c860b26bc588933be11113c1407a4d7c25f6b
insphere_mayank <insphere.mayank@gmail.com>
2018-04-04 10:16:03 +0530

    updates

---
+++ home.php
@@ -0,0 +1,39 @@
+<!-- Begin global header section -->
+<?php
+$pageTitle = 'GV Health';
+include("includes/_global-Head.php");
+?>
+<?php
+    include("includes/_header.php");
+?>
+
+<div class="clearfix"></div>
+<div class="banner__section image-covered" style="background-image: url('static/images/test-image.jpg')">
+    <div class="banner__section--ltArt"></div>
+    <div class="banner__section--rtArt">
+        <svg version="1.1" class="svg-inject" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             width="500px" height="500px" viewBox="0 0 500 500" enable-background="new 0 0 500 500" xml:space="preserve">
+            <path fill="#E79B24" d="M500,0v25.432v33.033v44.576v276.337V500h-92.567H174.199c0,0,16.892-84.835,43.919-135.135
+                  c0,0,63.813-131.381,85.961-156.156C304.079,208.709,397.125,65.315,500,0z"/>
+        </svg>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                sdsddd
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include("includes/_footer.php");
+?>
+<div class="clearfix"></div>
+<!-- Begin Script section -->
+<?php
+include("includes/_scripts.php");
+?>
+<!-- Begin Script section -->
+
+</body>
+</html>
